dd96a0cac2d1cf2b70ef7a4275ef4d524e1240f0
cyyyu <cyu9960@gmail.com>
2016-12-24 16:58:26 +0800

    count total keys

--- server/public/javascripts/index.js
+++ server/public/javascripts/index.js
@@ -5,10 +5,26 @@
     el: '#table-container',
     data: initdata
   })
+  TableView.Keyspace.totalKeys = countKeys(initdata.Keyspace)
 
+  /* socket io */
   var socket = io.connect()
-  socket.on('update', function(data) {
+  socket.on('update', updateViews)
+
+  /* funcs */
+  function updateViews(data) {
     Object.assign(TableView, data)
-  })
+
+    TableView.Keyspace.totalKeys = countKeys(data.Keyspace)
+  }
+
+  function countKeys(keyspace) {
+    var count = 0
+    for (var i in keyspace) {
+      if (typeof keyspace[i] === 'string')
+        count += parseInt(keyspace[i].trim().split(',')[0].split('=')[1])
+    }
+    return count
+  }
 
 })()
