a865382f4a36661d5d14c0399f117d56d540b3f5
Chuang Yu <cyu9960@gmail.com>
2017-01-31 00:07:35 +0800

    使用计算属性

--- server/public/javascripts/index.js
+++ server/public/javascripts/index.js
@@ -3,28 +3,34 @@
 
   var TableView = new Vue({
     el: '#table-container',
-    data: initdata
+    data: initdata,
+    computed: {
+      totalKeys: function() {
+        var vm = this
+        var keyspace = vm.Keyspace
+        var count = 0
+        for (var i in keyspace) {
+          if (typeof keyspace[i] === 'string')
+            count += parseInt(keyspace[i].trim().split(',')[0].split('=')[1])
+        }
+        return count
+      }
+    }
   })
-  TableView.Keyspace.totalKeys = countKeys(initdata.Keyspace)
 
   /* socket io */
   var socket = io.connect()
   socket.on('update', updateViews)
+  socket.on('updateHistory', updateCharts)
 
   /* funcs */
   function updateViews(data) {
     Object.assign(TableView, data)
-
-    TableView.Keyspace.totalKeys = countKeys(data.Keyspace)
   }
 
-  function countKeys(keyspace) {
-    var count = 0
-    for (var i in keyspace) {
-      if (typeof keyspace[i] === 'string')
-        count += parseInt(keyspace[i].trim().split(',')[0].split('=')[1])
-    }
-    return count
+  function updateCharts(history) {
+    /* Got history, update charts now. */
+    console.debug('got history: ', history)
   }
 
 })()
